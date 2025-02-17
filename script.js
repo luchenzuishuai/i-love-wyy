3ed13b506ea510871900b6a21ebaa63a250f4b4d
鹿琛 <1143729736@qq.com>
2025-02-17 21:21:21 +0800

    DONE

--- script.js
+++ script.js
@@ -13,9 +13,7 @@ let clickCount = 0;  // 记录点击 No 的次数
 const noTexts = [
     "？你认真的吗…", 
     "要不再想想？", 
-    "不许选这个！ ", 
-    "我会很伤心…", 
-    "不行:("
+    "给你脸了是吧！！不知好歹"
 ];
 
 // No 按钮点击事件
@@ -27,8 +25,8 @@ noButton.addEventListener("click", function() {
     yesButton.style.transform = `scale(${yesSize})`;
 
     // 挤压 No 按钮，每次右移 100px
-    let noOffset = clickCount * 50;
-    noButton.style.transform = `translateX(${noOffset}px)`;
+    let noOffset = clickCount * 50 /2;
+    noButton.style.transform = `translateY(${noOffset}px)`;
 
     // **新增：让图片和文字往上移动**
     let moveUp = clickCount * 25; // 每次上移 20px
@@ -36,24 +34,31 @@ noButton.addEventListener("click", function() {
     questionText.style.transform = `translateY(-${moveUp}px)`;
 
     // No 文案变化（前 5 次变化）
-    if (clickCount <= 5) {
+    if (clickCount <= 3) {
         noButton.innerText = noTexts[clickCount - 1];
+    } else {
+        noButton.innerText = noTexts[noTexts.length - 1]
     }
 
     // 图片变化（前 5 次变化）
     if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
     if (clickCount === 2) mainImage.src = "images/think.png";   // 思考
     if (clickCount === 3) mainImage.src = "images/angry.png";   // 生气
-    if (clickCount === 4) mainImage.src = "images/crying.png";  // 哭
-    if (clickCount >= 5) mainImage.src = "images/crying.png";  // 之后一直是哭
 
+    if (clickCount >= 3) {
+        noButton.disabled = true
+        noButton.style.cursor = 'not-allowed'
+    }
 });
 
 // Yes 按钮点击后，进入表白成功页面
 yesButton.addEventListener("click", function() {
     document.body.innerHTML = `
         <div class="yes-screen">
-            <h1 class="yes-text">!!!喜欢你!! ( >᎑<)♡︎ᐝ</h1>
+            <h1 class="yes-text">!!!喜欢你!! ( >᎑<)♡︎ᐝ
+             <br />
+             鹿琛❤王一伊
+              </h1>
             <img src="images/hug.png" alt="拥抱" class="yes-image">
         </div>
     `;
